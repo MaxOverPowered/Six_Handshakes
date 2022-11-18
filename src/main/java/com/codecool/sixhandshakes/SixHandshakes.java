a9fa5117a2d367f38105bd157f025d70eb62837e
Maxim Copita <copita.maxim.info@gmail.com>
2022-11-18 13:03:46 +0200

    Implement minimum distance and menu option in main

--- src/main/java/com/codecool/sixhandshakes/SixHandshakes.java
+++ src/main/java/com/codecool/sixhandshakes/SixHandshakes.java
@@ -1,13 +1,11 @@
 package com.codecool.sixhandshakes;
 
-import com.codecool.sixhandshakes.finders.FriendChainCalculator;
-import com.codecool.sixhandshakes.finders.FriendsOfFriendsFinder;
 import com.codecool.sixhandshakes.finders.HandshakeCalculator;
 import com.codecool.sixhandshakes.model.UserNode;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.Set;
 
 public class SixHandshakes {
@@ -15,10 +13,40 @@ public class SixHandshakes {
     private static GraphPlotter graphPlotter;
 
     public static void main(String[] args) {
+        UserNode start = null;
+        UserNode finish = null;
+//        boolean checher;
         initSocialGraph();
-        graphPlotter = new GraphPlotter(users);
 
+        int menuOption = getIntAsInput();
+        switch (menuOption) {
+            case 1:
+                String firstName1 = users.get(4).getFirstName();
+                String lastName1 = users.get(4).getLastName();
+                String firstName2 = users.get(7).getFirstName();
+                String lastName2 = users.get(7).getLastName();
+                for (int j = 0; j < users.size(); j++) {
+                    for (int i = 0; i < users.size(); i++) {
+                        if (Objects.equals(users.get(i).getFirstName(), firstName1) && Objects.equals(users.get(i).getLastName(), lastName1)) {
+                            if (Objects.equals(users.get(j).getFirstName(), firstName2) && Objects.equals(users.get(j).getLastName(), lastName2)) {
+                                start = users.get(i);
+                                finish = users.get(j);
+                            }
+                        }
+                    }
+                }
+
+                int count = HandshakeCalculator.getMinimumHandshakesBetween(start, finish);
+                System.out.println("Handshakes between " + start + " and " + finish + " is " + count + " ");
+                ;
+            case default:
+                System.out.println("There are no menu options, please try again ");
+
+        }
+
+        graphPlotter = new GraphPlotter(users);
         System.out.println("Done!");
+
     }
 
     private static void initSocialGraph() {
@@ -43,4 +71,23 @@ public class SixHandshakes {
         }
         System.out.println();
     }
+
+
+    private static int getIntAsInput() {
+        int choose = 0;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Choose from the menu: ");
+            System.out.println("1. Minimum handshakes");
+            choose = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Not valid form.");
+            getIntAsInput();
+        }
+
+        return choose;
+    }
+
+
 }
+
--- src/main/java/com/codecool/sixhandshakes/finders/HandshakeCalculator.java
+++ src/main/java/com/codecool/sixhandshakes/finders/HandshakeCalculator.java
@@ -2,10 +2,40 @@ package com.codecool.sixhandshakes.finders;
 
 import com.codecool.sixhandshakes.model.UserNode;
 
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Queue;
+import java.util.Set;
 
 public class HandshakeCalculator {
-    public static Optional<Integer> getMinimumHandshakesBetween(UserNode start, UserNode end) {
-        return Optional.empty();
+    public int counter = 0;
+    public static int getMinimumHandshakesBetween(UserNode start, UserNode end) {
+        int count = 0;
+        Queue<UserNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(start);
+        while (!nodeQueue.isEmpty()) {
+            int queueSize = nodeQueue.size();
+
+            while (queueSize != 0) {
+                UserNode currentNode = nodeQueue.element();
+                if (currentNode == end) {
+                    count++;
+                    return count;
+                }
+                Set<UserNode> friends = currentNode.getFriends();
+                for (UserNode currentFriend : friends) {
+                    if (currentFriend == end) {
+                        count++;
+                        return count;
+                    }
+                    nodeQueue.add(currentFriend);
+                }
+                queueSize--;
+                nodeQueue.poll();
+            }
+            count++;
+        }
+        return count;
     }
+
 }
